b1b5c59e9a09e67420b7aac2b9474421accdc23d
hridoykit <hridoy.tsa@gmail.com>
2024-09-23 15:57:57 +0600

    perform delete action

--- delete-confirmation-button.js
+++ delete-confirmation-button.js
@@ -5,9 +5,22 @@ document.getElementById('disable-input-field').addEventListener('keyup', (e) => 
     const button = document.getElementById('delete-btn');
 
     if(input.toLowerCase() === 'delete'){
-        button.disabled = false;
+        // button.disabled = false;
+        button.removeAttribute('disabled');
     }
     else{
-        button.disabled = true;
+        // button.disabled = true;
+        button.setAttribute('disabled', true);
+    }
+});
+
+// delete the text
+document.getElementById('delete-btn').addEventListener('click', () => {
+    if(confirm('want to delete?')){
+        document.getElementById('delete-text').style.display = 'none'; //perform delete action
+        alert('item deleted!');
+    }
+    else{
+        alert('delete action canceled')
     }
 });
