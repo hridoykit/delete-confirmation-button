9387b1b5260de37f5cd9ebad552174adb8718392
hridoykit <hridoy.tsa@gmail.com>
2024-09-23 16:41:11 +0600

    enable the delete button

--- delete-confirmation-button.js
+++ delete-confirmation-button.js
@@ -1,4 +1,13 @@
 document.getElementById('disable-input-field').addEventListener('keyup', (e) => {
     e.preventDefault();
-    console.log(e.target.value);
+    // const inputValue = document.getElementById('disable-input-field').value;
+    const input = e.target.value;
+    const button = document.getElementById('delete-btn');
+
+    if(input.toLowerCase() === 'delete'){
+        button.disabled = false;
+    }
+    else{
+        button.disabled = true;
+    }
 });
